786ae00734410216df84be02c14441c61deb91f1
osama adel <osama.adel7358@gmail.com>
2025-03-13 11:38:23 +0200

    .

--- zz.py
+++ zz.py
@@ -5,10 +5,11 @@ llm_response = open('./zideal.md', 'r').read()
 # Regular expression to match the file path and code block content
 pattern = r"```([^\n]*)\n(.*?)```"
 
+
 matches = re.findall(pattern, llm_response, re.DOTALL)
 
 for match in matches:
-    file_path = match[0]
+    file_path = match[0].replace('javascript', 'js')
     code_content = match[1]
     
     # Write new content to the file
@@ -20,9 +21,12 @@ for match in matches:
     except Exception as e:
         raise Exception(f"Error writing to {file_path}: {str(e)}")
    
+ 
    
-package = ''.join(open('./zpackage.md', 'r').readlines()[1:-1])
-open( './package.json', 'w' ).write(  package)
+package = ''.join(open('./zpackage.md', 'r').read())
+matches = re.findall(pattern, package, re.DOTALL)[0]
+open( './package.json', 'w' ).write(  matches[1])
 
-test = ''.join(open('./ztest.md', 'r').readlines()[1:-1])
-open( './solution.test.js', 'w' ).write(  test)
+test = ''.join(open('./ztest.md', 'r').read())
+matches = re.findall(pattern, test, re.DOTALL)[0]
+open( './solution.test.js', 'w' ).write(  matches[1])
